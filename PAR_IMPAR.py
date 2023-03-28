e42348daea2e3a56db7da6c30f6d19c76684bb53
Elkin2234513 <126176086+Elkin2234513@users.noreply.github.com>
2023-03-28 00:34:40 -0500

    Add files via upload

---
+++ PAR_IMPAR.py
@@ -0,0 +1,15 @@
+# par o impar
+print("-------------------------------")
+print("---------PAR O IMPAR-----------")
+print("-------------------------------")
+
+n=int(input("Digite n:"))
+r=n%2
+if r==0:
+    rta= "PAR"
+else:
+    rta= "IMPAR"
+
+print("-------------------------------")
+print("El numero es " +str(rta))
+print("-------------------------------")
